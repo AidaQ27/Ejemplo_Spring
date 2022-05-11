3f08e80b5e0b9da42851ae06dd2642c0ac9c0002
AidaQ27 <aidaqueraltt@gmail.com>
2022-05-11 12:50:53 +0200

    Project Gradle

---
+++ PEjemplo-1/src/main/java/com/example/demo/PEjemplo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PEjemplo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PEjemplo1Application.class, args);
+	}
+
+}
